a2c0ec5a30d9bd127c56438c0dd85db4f9ff055e
James Sanford <jamesthaimassage@gmail.com>
2013-03-07 19:41:12 +0000

    ShareKit updated to the newest version

---
+++ Classes/InstantWildSHKConfigurator.h
@@ -0,0 +1,13 @@
+//
+//  InstantWildSHKConfigurator.h
+//  instantWild
+//
+//  Created by James Sanford on 06/03/2013.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface InstantWildSHKConfigurator : NSObject
+
+@end
---
+++ Classes/InstantWildSHKConfigurator.m
@@ -0,0 +1,13 @@
+//
+//  InstantWildSHKConfigurator.m
+//  instantWild
+//
+//  Created by James Sanford on 06/03/2013.
+//
+//
+
+#import "InstantWildSHKConfigurator.h"
+
+@implementation InstantWildSHKConfigurator
+
+@end
