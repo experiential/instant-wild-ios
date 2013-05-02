d9e0862f2ce6eab7b0344d8b9f16fe886181c824
James Sanford <jamesthaimassage@gmail.com>
2013-05-02 14:08:06 +0100

    ShareKit updated to the newest version

---
+++ Classes/CameraData.h
@@ -0,0 +1,13 @@
+//
+//  CameraData.h
+//  instantWild
+//
+//  Created by James Sanford on 13/03/2013.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CameraData : NSObject
+
+@end
---
+++ Classes/CameraData.m
@@ -0,0 +1,13 @@
+//
+//  CameraData.m
+//  instantWild
+//
+//  Created by James Sanford on 13/03/2013.
+//
+//
+
+#import "CameraData.h"
+
+@implementation CameraData
+
+@end
